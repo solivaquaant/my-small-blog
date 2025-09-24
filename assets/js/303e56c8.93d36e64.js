71ce303c3f64fcd248994e53795e673474297c28
solivaquaant <solivaquaant@users.noreply.github.com>
2025-09-24 09:15:13 +0000

    deploy: 22f5d93ae33673954fdaef3d76d5d00edc0ef144

--- assets/js/303e56c8.93d36e64.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_own_blog=self.webpackChunkmy_own_blog||[]).push([[7146],{4596:l=>{l.exports=JSON.parse('{"metadata":{"permalink":"/my-small-blog/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/36994c47.e6ff39de.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_own_blog=self.webpackChunkmy_own_blog||[]).push([[9858],{45516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
